e01036db1bfca6297062e69628aa938cdbf1644b
guzmandrade-wds <mauricio.andrade@webdevstudios.com>
2023-09-02 20:26:31 -0500

    code doc block explaning how to use ACF with this files

--- inc/wpcli/block-starter/block.php
+++ inc/wpcli/block-starter/block.php
@@ -5,6 +5,8 @@
  * @package wd_f
  */
 
+// ACF (Pro or Free) can be used here since we are loading this block using <ServerSideRender> (for the Free version), and adding this file as ACF render template.
+// However, if using the free version of ACF, you would need to refresh the edit page to see your changes on the edit page.
 ?>
 <p <?php echo esc_attr( get_block_wrapper_attributes() ); ?>>
 	<?php esc_html_e( 'Example Dynamic - hello from a dynamic block!', 'wd_f' ); ?>
