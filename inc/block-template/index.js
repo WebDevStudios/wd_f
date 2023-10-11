d56bc1c4d50f566f6187344dc2ea352b141c6e87
JC <jc@webdevstudios.com>
2023-10-12 05:34:51 +0800

    WDSUS-46 - Remove options from script and add default to template instead

--- inc/block-template/index.js
+++ inc/block-template/index.js
@@ -2,19 +2,23 @@ const { join } = require( 'path' );
 
 module.exports = {
 	defaultValues: {
+		description: 'The Block Description',
+		author: 'WebDevStudios',
+		category: 'wds-blocks-category',
+		dashicon: 'pets',
+		namespace: 'wds',
 		folderName: 'src',
 		editorScript: 'file:./index.js',
 		editorStyle: 'file:./index.css',
 		style: 'file:./style-index.css',
 		viewScript: 'file:./view.js',
-		main: 'file:./index.js',
+		example: {},
+		supports: {
+			html: false,
+		},
+		attributes: {},
 		customScripts: {
 			build: 'wp-scripts build',
-			format: 'wp-scripts format',
-			'lint:css': 'wp-scripts lint-style',
-			'lint:js': 'wp-scripts lint-js',
-			'packages-update': 'wp-scripts packages-update',
-			'plugin-zip': 'wp-scripts plugin-zip',
 			start: 'wp-scripts start',
 		},
 		devDependencies: {
